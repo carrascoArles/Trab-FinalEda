04c965db7c8d409add2e220f0ef5010c91834881
carrascoArles <acarrascoch@unsa.edu.pe>
2023-08-06 15:23:08 -0500

    agregando el setResult y el getResult

--- ResultArchivo.java
+++ ResultArchivo.java
@@ -5,5 +5,11 @@ public class ResultArchivo {
         this.result = new boolean[n];
     }
 
-    
+    public void setResult(int index, boolean isPlagiarized) {
+        this.result[index] = isPlagiarized;
+    }
+
+    public boolean[] getResult() {
+        return this.result;
+    }
 }
