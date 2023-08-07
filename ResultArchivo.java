f242c19b43b6a6888f0f78ffcd49416ed1d50371
carrascoArles <acarrascoch@unsa.edu.pe>
2023-08-06 19:22:40 -0500

    agregando mas info a los constructores y tambien los getfiles

--- ResultArchivo.java
+++ ResultArchivo.java
@@ -1,15 +1,31 @@
 public class ResultArchivo {
-    private boolean[] result;
+    private int n;
+    private boolean[] results;
+    private String[] fileNames;
+    private String[] plagiarismInfo;
 
     public ResultArchivo(int n) {
-        this.result = new boolean[n];
+        this.n = n;
+        results = new boolean[n];
+        fileNames = new String[n];
+        plagiarismInfo = new String[n];
     }
 
-    public void setResult(int index, boolean isPlagiarized) {
-        this.result[index] = isPlagiarized;
+    public void setResult(int index, boolean result, String fileName, String plagiarismInfo) {
+        results[index] = result;
+        fileNames[index] = fileName;
+        this.plagiarismInfo[index] = plagiarismInfo;
     }
 
     public boolean[] getResult() {
-        return this.result;
+        return results;
+    }
+
+    public String[] getFileNames() {
+        return fileNames;
+    }
+
+    public String[] getPlagiarismInfo() {
+        return plagiarismInfo;
     }
 }
