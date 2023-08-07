eee53c528c0b99361f9df655f4e707b789deb417
carrascoArles <acarrascoch@unsa.edu.pe>
2023-08-06 19:45:49 -0500

    a

---
+++ Detectordeplagio/Trie.java
@@ -0,0 +1,33 @@
+package Detectordeplagio;
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode current = root;
+        for (char ch : word.toCharArray()) {
+            int index = ch - 'a';
+            if (current.getChild(ch) == null) {
+                current.addChild(ch, new TrieNode());
+            }
+            current = current.getChild(ch);
+        }
+        current.setEndOfWord(true);
+    }
+
+    public boolean search(String word) {
+        TrieNode current = root;
+        for (char ch : word.toCharArray()) {
+            int index = ch - 'a';
+            TrieNode node = current.getChild(ch);
+            if (node == null) {
+                return false;
+            }
+            current = node;
+        }
+        return current.isEndOfWord();
+    }
+}
---
+++ Detectordeplagio/TrieNode.java
@@ -0,0 +1,33 @@
+package Detectordeplagio;
+class TrieNode{
+    private TrieNode[] children;
+    private boolean isEndOfWord;
+
+    public TrieNode() {
+        children = new TrieNode[26];
+        isEndOfWord = false;
+    }
+    
+    public boolean isEndOfWord() {
+        return isEndOfWord;
+    }
+
+    public void setEndOfWord(boolean endOfWord) {
+        isEndOfWord = endOfWord;
+    }
+     
+    public TrieNode getChild(char ch) {
+        int index = ch - 'a';
+        if (index >= 0 && index < 26) {
+            return children[index];
+        }
+        return null;
+    }
+
+    public void addChild(char ch, TrieNode node) {
+        int index = ch - 'a';
+        if (index >= 0 && index < 26) {
+            children[index] = node;
+        }
+    }
+}
