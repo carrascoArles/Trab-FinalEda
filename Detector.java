e1892ec5aa02ab8a08b9068f3fa3cd1eaa45eb68
carrascoArles <acarrascoch@unsa.edu.pe>
2023-08-06 19:31:11 -0500

    agregando el verifyPlagiarism para llamar al otro metodo

--- Detector.java
+++ Detector.java
@@ -25,4 +25,19 @@ public class Detector {
             trie.insert(word);
         }
     }
+
+    public ResultArchivo verifyPlagiarism(String textToCheck) {
+        String[] wordsToCheck = textToCheck.toLowerCase().split("\\s+");
+        int n = database.size();
+        ResultArchivo resultChecker = new ResultArchivo(n);
+
+        for (int i = 0; i < n; i++) {
+            boolean isPlagiarized = isTextPlagiarized(wordsToCheck, database.get(i));
+            String fileName = "Archivo " + (i + 1);
+            String plagiarismInfo = isPlagiarized ? "Plagio detectado" : "Sin plagio";
+            resultChecker.setResult(i, isPlagiarized, fileName, plagiarismInfo);
+        }
+
+        return resultChecker;
+    }
 }
