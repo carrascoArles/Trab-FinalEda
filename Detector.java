ccf37c13c99ff4e8e5a307b86140a29bb5acebb2
carrascoArles <acarrascoch@unsa.edu.pe>
2023-08-06 19:25:39 -0500

    agregando loadFiles para subir archivos

--- Detector.java
+++ Detector.java
@@ -1,13 +1,21 @@
-
 public class Detector {
-    private List<String> database;
+    Trie trie;
+    List<List<String>> database;
 
     public Detector() {
+        trie = new Trie();
         database = new ArrayList<>();
     }
 
     public void loadFiles(List<String> files) {
-        database = files;
+        for (String fileContent : files) {
+            insertWordsToTrie(fileContent);
+            List<String> fileWords = new ArrayList<>();
+            String[] words = fileContent.toLowerCase().split("\\s+");
+            for (String word : words) {
+                fileWords.add(word);
+            }
+            database.add(fileWords);
+        }
     }
-
-    }
+}
